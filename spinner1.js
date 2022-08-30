978c3eb83e2de680bd22d83fe8885f05f8b03e27
Jordan Gertenstein <jordangert@hotmail.com>
2022-08-30 14:28:00 -0500

    add spinner.js

---
+++ spinner1.js
@@ -0,0 +1,26 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2000);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 3000);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r|   '); 
+}, 4000);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write(''); 
+}, 5000);
